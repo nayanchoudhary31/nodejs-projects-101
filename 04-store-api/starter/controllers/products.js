b98a8ef0586874d9c7ed3c813587af398042d5c0
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-05-20 00:34:55 +0530

    add req.query

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,9 +1,17 @@
+const Products = require("../models/product");
 const getAllStaticProducts = async (req, resp) => {
-  resp.status(200).json({ msg: "All Static Products" });
+  const products = await Products.find({ featured: true });
+  resp.status(200).json({ products: products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, resp) => {
-  resp.status(200).json({ msg: "All Products" });
+  const { featured } = req.query;
+  let queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  const products = await Products.find(queryObject);
+  resp.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllStaticProducts, getAllProducts };
