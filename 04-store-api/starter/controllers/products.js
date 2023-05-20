57b295f5504876d19e9d86625890ce532e32293a
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-05-21 00:40:12 +0530

    Implemetin Regex and Sort in API

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,14 +1,20 @@
 const Products = require("../models/product");
 const getAllStaticProducts = async (req, resp) => {
-  const products = await Products.find({ featured: true });
+  const products = await Products.find({}).sort("-price -name");
   resp.status(200).json({ products: products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, resp) => {
-  const { featured } = req.query;
+  const { featured, company, name } = req.query;
   let queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" }; // used for string pattern match option i is Case Sensative
   }
   const products = await Products.find(queryObject);
   resp.status(200).json({ products, nbHits: products.length });
