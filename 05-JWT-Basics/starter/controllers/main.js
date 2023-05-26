820800afb7c84ba69ba10c16468e39303c94f665
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-05-26 23:41:11 +0530

    Added Controller Routes

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,13 +1,13 @@
 const login = async (req, resp) => {
-    return resp.status(200).json({ msg: "User Login Successfully" });
-  };
-  
-  const dashboard = async (req, resp) => {
-    const luckyNumber = Math.floor(Math.random() * 100);
-    return resp
-      .status(200)
-      .json({ msg: `Here is your lucky number ${luckyNumber}` });
-  };
-  
-  module.exports = { login, dashboard };
-  
+  return resp.status(200).json({ msg: "User Login Successfully" });
+};
+
+const dashboard = async (req, resp) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+  return resp.status(200).json({
+    msg: `Hello Nayan`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
+};
+
+module.exports = { login, dashboard };
--- 05-JWT-Basics/starter/routes/main.js
+++ 05-JWT-Basics/starter/routes/main.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { dashboard, login } = require("../controllers/main");
-const { model } = require("mongoose");
 const router = express.Router();
 
 router.route("/dashboard").get(dashboard);
