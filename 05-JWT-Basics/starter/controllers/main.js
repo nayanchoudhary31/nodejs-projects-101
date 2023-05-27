268816e8c4abf0e849db6d392468fe5338159753
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-05-28 00:17:03 +0530

    JWT Token

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,5 +1,27 @@
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
 const login = async (req, resp) => {
-  return resp.status(200).json({ msg: "User Login Successfully" });
+  const { username, password } = req.body;
+
+  // Validation for Username & Password
+  if (!username || !password) {
+    throw new CustomAPIError("Username & Password is Required", 400);
+  }
+
+  // Generally we used DB Id
+  const id = new Date().getDate();
+
+  // Generate JWT token with payload
+  // Don't send private information in payload
+  // Try to keep your payload for better user experience
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  return resp
+    .status(200)
+    .json({ msg: "User Login Successfully", token: token });
 };
 
 const dashboard = async (req, resp) => {
