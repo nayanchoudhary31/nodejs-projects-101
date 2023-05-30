b79d72859f3a33e88aa110509d706a74613a44ef
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-05-30 22:58:57 +0530

    Added Auth Middleware

--- 05-JWT-Basics/starter/middleware/auth.js
+++ 05-JWT-Basics/starter/middleware/auth.js
@@ -1,5 +1,19 @@
+const jwt = require("jsonwebtoken");
+
 const authenticationMiddleWare = async (req, resp, next) => {
-  console.log(req.headers.authorization);
+  const authHeaders = req.headers.authorization;
+  if (!authHeaders || !authHeaders.startsWith("Bearer ")) {
+    throw new CustomAPIError("Token is required!", 401);
+  }
+  // Get the token
+  const token = authHeaders.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { id, username } = decoded;
+    req.user = { id, username };
+    next();
+  } catch (error) {}
   next();
 };
 
