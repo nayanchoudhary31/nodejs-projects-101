01a2df767c7df91036b046509c3b0564951f31fe
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-06-01 23:12:52 +0530

    Updated Jobs API

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -1,18 +1,19 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
 const app = express();
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const connectDB = require("./db/connect");
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
+app.get("/", (req, res) => {
+  res.send("jobs api");
 });
 
 app.use(notFoundMiddleware);
@@ -22,6 +23,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
